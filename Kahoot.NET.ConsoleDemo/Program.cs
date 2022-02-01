2eb99049a446cf7368c344a9a82e48dfde8d81dd
Bradley Grover <ac111897@avcol.school.nz>
2022-02-01 18:37:14 +1300

    Create console testing to test internal functions and visualise easier

---
+++ Kahoot.NET.ConsoleDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using Kahoot.NET;
+using Kahoot.NET.Client;
+
+namespace Kahoot.NET.ConsoleDemo;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        int code = Convert.ToInt32(Console.ReadLine());
+
+        KahootClient client = new(new HttpClient());
+
+        client.GameId = code;
+
+        await client.CreateHandshakeAsync();
+    }
+}

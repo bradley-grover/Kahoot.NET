e74f727cab2bf6c3dca23f7c2765b4087e6b0cd3
Bradley Grover <ac111897@avcol.school.nz>
2022-02-01 18:39:50 +1300

    Add classes and methods to initialize connection with kahoot websocket

---
+++ Kahoot.NET/Internals/Connection/ConnectionHelper.cs
@@ -0,0 +1,41 @@
+﻿namespace Kahoot.NET.Internals.Connection;
+
+/// <summary>
+/// Contains static methods to help create a connection to the kahoot game
+/// </summary>
+internal static class ConnectionHelper
+{
+    /// <summary>
+    /// The header for the session token
+    /// </summary>
+    private const string SessionHeader = "x-kahoot-session-token";
+    /// <summary>
+    /// Session
+    /// </summary>
+    private const string SessionURL = "https://kahoot.it/reserve/session/{0}";
+    /// <summary>
+    /// Gets a reponse from Kahoot about the game code
+    /// </summary>
+    /// <param name="gameId"></param>
+    /// <param name="client"></param>
+    /// <returns></returns>
+    /// <exception cref="GameNotFoundException"></exception>
+    internal static async Task<(CreateSessionResponse? response, string? header)> CreateSessionResponseAsync(int gameId, HttpClient client)
+    {
+        var data = await client.GetAsync(string.Format(SessionURL, gameId));
+
+        if (!data.IsSuccessStatusCode)
+        {
+            throw new GameNotFoundException();
+        }
+
+        var content = await data.Content.ReadAsStringAsync();
+
+        if (!data.Headers.TryGetValues(SessionHeader, out var headers))
+        {
+            throw new NoSessionHeaderFoundException(); 
+        }
+
+        return (JsonSerializer.Deserialize<CreateSessionResponse>(content), headers.FirstOrDefault());
+    }
+}
---
+++ Kahoot.NET/Internals/Connection/Token.cs
@@ -0,0 +1,53 @@
+﻿using System.Text.RegularExpressions;
+using System.Text;
+
+namespace Kahoot.NET.Internals.Connection;
+
+internal static class Token
+{
+    public static async Task<string> CreateTokenAsync(int id, HttpClient client)
+    {
+        var (response, header) = await ConnectionHelper.CreateSessionResponseAsync(id, client);
+
+        if (response is null)
+        {
+            throw new GameNotFoundException();
+        }
+
+        return CombineTokens(header, response.ParseChallenge());
+    }
+    private static string ParseChallenge(this CreateSessionResponse response) => Decode(response.Challenge!);
+
+    private static string CombineTokens(ReadOnlySpan<char> header, ReadOnlySpan<char> challenge)
+    {
+        StringBuilder builder = new(header.Length);
+
+        for (int i = 0;  i < header.Length; i++)
+        {
+            char c = header[i];
+            char mod = challenge[i % challenge.Length];
+            int decoded = c ^ mod;
+            builder.Append(Convert.ToChar(decoded));
+        }
+        
+        return builder.ToString();
+    }
+
+    private static string Decode(string challenge)
+    {
+        int offset = ((41 * 39) * (100 + 11 * 9));
+
+        char repl(char value, int position)
+        {
+            return Convert.ToChar(((value * position + offset) % 77) + 48);
+        }
+        StringBuilder builder = new(string.Empty);
+
+        for (int i = 0; i < challenge.Length; i++)
+        {
+            builder.Append(repl(challenge[i], i));
+        }
+        return builder.ToString();
+    }
+
+}

557dc5110c379b50da5fba8c6c82d1e4a0bc705e
Bradley Grover <ac111897@avcol.school.nz>
2022-02-04 01:24:09 +1300

    Add another exception where it throws if it was unable to parse the challenge

---
+++ Kahoot.NET/Exceptions/InvalidChallangeException.cs
@@ -0,0 +1,13 @@
+﻿namespace Kahoot.NET.Exceptions;
+
+
+[Serializable]
+public class InvalidChallengeException : Exception
+{
+    public InvalidChallengeException() { }
+    public InvalidChallengeException(string message) : base(message) { }
+    public InvalidChallengeException(string message, Exception inner) : base(message, inner) { }
+    protected InvalidChallengeException(
+      System.Runtime.Serialization.SerializationInfo info,
+      System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+}

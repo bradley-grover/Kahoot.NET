b9ef21c93f4705354f7c51baf4f61cd4d55b85b3
Bradley Grover <ac111897@avcol.school.nz>
2022-02-04 01:26:37 +1300

    Some demo testing to see if stuff works and visualise it

--- Kahoot.NET.ConsoleDemo/Program.cs
+++ Kahoot.NET.ConsoleDemo/Program.cs
@@ -1,5 +1,8 @@
 ﻿using Kahoot.NET;
 using Kahoot.NET.Client;
+using Kahoot.NET.Internals.Connection;
+using Kahoot.NET.Internals.Connection.Parsers;
+using Kahoot.NET.Internals.Connection.Token;
 
 namespace Kahoot.NET.ConsoleDemo;
 
@@ -7,12 +10,38 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
+        Console.WriteLine("ENTER CODE");
         int code = Convert.ToInt32(Console.ReadLine());
 
-        KahootClient client = new(new HttpClient());
+        var kahoot = new KahootClient(new HttpClient());
 
-        client.GameId = code;
+        await kahoot.JoinAsync(code, "ok");
+        await kahoot.CreateHandshakeAsync();
 
-        await client.CreateHandshakeAsync();
+        //await RunConnectionTestAsync(code);
+
+    }
+
+    public static async Task RunConnectionTestAsync(int code)
+    {
+        var (res, header) = await ConnectionHelper.CreateSessionResponseAsync(code, new());
+
+        Console.WriteLine($"Header before transformation:\n\n {header}");
+
+        var head = HeaderToken.CreateHeaderToken(header).ToString();
+
+        Console.WriteLine($"Header After:\n\n{head}");
+
+
+        Console.WriteLine($"Challenge before transformation:\n\n {res.Challenge}");
+
+        Console.WriteLine($"\nOffset calculated:\n{new OffsetParser().Parse(res.Challenge.RemoveWhitespace())}");
+
+
+        Console.WriteLine(ChallengeToken.CreateToken(res.Challenge).ToString());
+
+
+        var result = await Token.CreateTokenAsync(code, new());
+        Console.WriteLine(result.ToString());
     }
 }

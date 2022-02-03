ea1d90121d85a4906254804b14068b0963bb1b48
Bradley Grover <ac111897@avcol.school.nz>
2022-02-04 01:27:37 +1300

    Update global usings

--- Kahoot.NET/GlobalUsings.cs
+++ Kahoot.NET/GlobalUsings.cs
@@ -5,8 +5,11 @@ global using Kahoot.NET.Question;
 global using Kahoot.NET.Internals;
 global using Kahoot.NET.Internals.Responses;
 global using Kahoot.NET.Internals.Connection;
+global using Kahoot.NET.Internals.Connection.Parsers;
+global using Kahoot.NET.Internals.Connection.Token;
 global using Kahoot.NET.Exceptions;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
+global using System.Runtime.CompilerServices;
 global using OneOf;
 

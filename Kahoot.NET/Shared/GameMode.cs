2b9755c405da87a85fe0e5060aecf52c275d1431
Bradley Grover <ac111897@avcol.school.nz>
2022-02-07 16:57:58 +1300

    add commenting to game mode

--- Kahoot.NET/Shared/GameMode.cs
+++ Kahoot.NET/Shared/GameMode.cs
@@ -1,7 +1,16 @@
 ﻿namespace Kahoot.NET.Shared;
 
+/// <summary>
+/// The gamemode of the quiz selected
+/// </summary>
 public enum GameMode
 {
+    /// <summary>
+    /// The quiz is played by solo players
+    /// </summary>
     Classic,
+    /// <summary>
+    /// The quiz is played by teams
+    /// </summary>
     Team
 }

868094a11d51144b112e8adcf7f8ce76846f5682
Bradley Grover <ac111897@avcol.school.nz>
2022-02-07 16:47:16 +1300

    Add more usings

--- Kahoot.NET/GlobalUsings.cs
+++ Kahoot.NET/GlobalUsings.cs
@@ -1,15 +1,23 @@
-﻿global using Kahoot.NET.Client;
+﻿#region ProjectLibraries
+global using Kahoot.NET.Client;
 global using Kahoot.NET.Quiz;
 global using Kahoot.NET.Shared;
 global using Kahoot.NET.Question;
 global using Kahoot.NET.Internals;
-global using Kahoot.NET.Internals.Responses;
-global using Kahoot.NET.Internals.Connection;
-global using Kahoot.NET.Internals.Connection.Parsers;
-global using Kahoot.NET.Internals.Connection.Token;
 global using Kahoot.NET.Exceptions;
+global using Kahoot.NET.Internals.Responses;
+global using Kahoot.NET.Internals.Parsers;
+global using Kahoot.NET.Internals.Connection.Token;
+#endregion
+#region Standard Libraries
+global using System.Text;
 global using System.Text.Json;
 global using System.Text.Json.Serialization;
 global using System.Runtime.CompilerServices;
+global using System.Net.WebSockets;
+global using Microsoft.Extensions.Logging;
+#endregion
+#region External Libraries
 global using OneOf;
+#endregion
 

202c647ebb7e677a63dd47f671b75cb041709630
Bradley Grover <ac111897@avcol.school.nz>
2022-02-07 16:58:17 +1300

    add async event handler for future purposes

---
+++ Kahoot.NET/Shared/AsyncEventHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace Kahoot.NET.Shared;
+
+/// <summary>
+/// Asynchrounous event handler to await a handler
+/// </summary>
+/// <param name="sender">sender</param>
+/// <param name="e">Event Args</param>
+/// <returns>A <see cref="Task"/></returns>
+
+public delegate Task AsyncEventHandler(object? sender, EventArgs e);
+/// <summary>
+/// Asynchrounous event handler to await a handler
+/// </summary>
+/// <typeparam name="TEventArgs">The type of <see cref="EventArgs"/></typeparam>
+/// <param name="sender">sender</param>
+/// <param name="e">Event Args to be passed</param>
+/// <returns>A <see cref="Task"/></returns>
+public delegate Task AsyncEventHandler<TEventArgs>(object? sender, TEventArgs e)
+    where TEventArgs : EventArgs;

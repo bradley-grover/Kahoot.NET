547cf404fbd82732f634ac204307883f18be857b
Bradley Grover <ac111897@avcol.school.nz>
2022-02-07 16:54:34 +1300

    add messages to send to websocket

---
+++ Kahoot.NET/Internals/Messages/IMessageBase.cs
@@ -0,0 +1,13 @@
+﻿namespace Kahoot.NET.Internals.Messages;
+
+/// <summary>
+/// Interface to provide a way of sending data to the Kahoot websocket
+/// </summary>
+public interface IMessageBase
+{
+    /// <summary>
+    /// Returns the message in a data format to send to Kahoot
+    /// </summary>
+    /// <returns></returns>
+    public ReadOnlyMemory<byte> ToRawMessage();
+}

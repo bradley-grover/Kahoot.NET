adac0fb525324d0c6ddc8cedab1e4dc346013483
Bradley Grover <ac111897@avcol.school.nz>
2022-02-16 20:27:39 +1300

    Update console demo

--- Kahoot.NET.ConsoleDemo/Program.cs
+++ Kahoot.NET.ConsoleDemo/Program.cs
@@ -11,6 +11,8 @@ using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Kahoot.NET.Internals.Messages.Handshake;
+using Kahoot.NET.Internals.Responses;
+using System.Diagnostics;
 
 namespace Kahoot.NET.ConsoleDemo;
 
@@ -18,54 +20,12 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        await RunConnectionTestAsync(int.Parse(Console.ReadLine()!));
+        IKahootClient client = new KahootClient(KahootClientConfig.Default, new HttpClient());
+
+        int code = int.Parse(Console.ReadLine()!);
+
+
+        await client.JoinAsync(code, "nice");
     }
 
-    public static async Task RunConnectionTestAsync(int code)
-    {
-        string token = await Token.CreateTokenAsync(code, new());
-
-        ClientWebSocket socket = new();
-
-        Uri uri = new("wss://kahoot.it/cometd/7841538/2d36304551208acb52724709ff04f6c451d08d0f985e70049af7821e9ef92e89d9c876cfb82aa0bfdd6105388fc5a58e");
-
-        Uri other = new Uri(string.Format("wss://kahoot.it/cometd/{0}/{1}", code, token));
-
-        Console.WriteLine(other);
-
-        await socket.ConnectAsync(uri, CancellationToken.None);
-
-        Memory<byte> buffer = new byte[512];
-
-        string content = JsonSerializer.Serialize(new FirstHandshake());
-
-        var message = new ArraySegment<byte>(Encoding.UTF8.GetBytes(content));
-
-        await socket.SendAsync(message, WebSocketMessageType.Text, true, CancellationToken.None);
-
-        while (socket.State == WebSocketState.Open)
-        {
-
-            var res = await socket.ReceiveAsync(buffer, CancellationToken.None);
-            if (res.MessageType == WebSocketMessageType.Close)
-            {
-                await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
-            }
-            Content(buffer, res.Count);
-        }
-
-    }
-
-    public static void Content(Memory<byte> data, int count)
-    {
-        Console.WriteLine(Encoding.UTF8.GetString(data.ToArray(), 0, count));
-    }
-
-    private static void WriteLineEmpty(int count)
-    {
-        for (int i = 0; i < count; i++)
-        {
-            Console.WriteLine();
-        }
-    }
 }

bbe4f5bebdf5de1927781f663acb9a6e462ae33f
Bradley Grover <ac111897@avcol.school.nz>
2022-05-02 22:57:45 +1200

    add support for leaving kahoots, not finished yet

---
+++ Kahoot.NET/Internal/Data/Messages/Leaving/LiveLeaveMessage.cs
@@ -0,0 +1,11 @@
+﻿using Kahoot.NET.Internal.Data.Shared.Ext;
+
+namespace Kahoot.NET.Internal.Data.Messages.Leaving;
+
+#nullable disable
+
+internal class LiveLeaveMessage : LiveMessage
+{
+    [JsonPropertyName("ext")]
+    public ExtWithTimesync<long> Ext { get; set; }
+}

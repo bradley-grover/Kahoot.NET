b7385012551dfc82b3e585b0a8a53933e58c5039
Bradley Grover <ac111897@avcol.school.nz>
2022-07-14 23:24:57 +0200

    fix overflowed integers in datatable to allow for high performance

--- Kahoot.NET.Tests/ParserTests.cs
+++ Kahoot.NET.Tests/ParserTests.cs
@@ -9,6 +9,9 @@ public class ParserTests
     [InlineData("2+2", 4)]
     [InlineData("(2*2)*4", 16)]
     [InlineData("((2*5)+(2*2))", 14)]
+    [InlineData("(60*20*50)+(20*2)", 60_040)]
+    [InlineData("(90*30*68)*(80*80*20*30)", 705024000000)]
+    [InlineData("(90*90*90*90*90)+(10*8)", 5_904_900_080)]
     public void AssertThatParser_Works(string expression, long expected)
     {
         IValueParser<long> parser = new OffsetArithmetic();
--- Kahoot.NET/Parsers/OffsetArithmetic.cs
+++ Kahoot.NET/Parsers/OffsetArithmetic.cs
@@ -1,18 +1,40 @@
 ﻿using System.Data;
+using System.Text.RegularExpressions;
 
 namespace Kahoot.NET.Parsers;
+
+/*
+ * TODO: Faster implementation of calculating offset
+ * 
+ * .NET 7 adds support for Regex Source Generators so convert code to use that
+ * Hopefully span overloads are added as well
+ * 
+ * The reason this class uses DataTable.Compute instead of a expression parser is because it is way cheaper
+ * Flee was used initially but allocates about 10x more memory and about 10x slower as well
+ */
 
 /// <summary>
 /// Calculate the offset for the function to calculate the challenge string
 /// </summary>
 internal class OffsetArithmetic : IValueParser<long>
 {
+    // TODO: find better expression for matching
+    private static Regex InternalRegex { get; } = new(@"\d+(\.\d+)?", RegexOptions.Compiled);
     private static DataTable Table { get; } = new();
 
     public long Parse(ReadOnlySpan<char> input)
     {
-        var result = Table.Compute(input.ToString(), null);
+        var result = Table.Compute(SanitiseToDecimal(input).ToString(), null);
 
-        return (long)(int)result;
+        return Convert.ToInt64((decimal)result);
+    }
+
+    internal static ReadOnlySpan<char> SanitiseToDecimal(ReadOnlySpan<char> value)
+    {
+        return InternalRegex.Replace(value.ToString(), m =>
+        {
+            var slice = m.ValueSpan;
+            return slice.Contains('.') ? slice.ToString() : string.Format("{0}.0", slice.ToString());
+        });
     }
 }

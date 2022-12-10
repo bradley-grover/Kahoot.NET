d3073fa578a086c2d2feba08afd0ecb557db2897
Bradley Grover <ac111897@avcol.school.nz>
2022-12-11 01:35:12 +1300

    Add decode benchmark

---
+++ Kahoot.NET.Benchmarks/ToRun/DecodeOnly.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Kahoot.NET.API.Authentication;
+
+namespace Kahoot.NET.Benchmarks.ToRun;
+
+[BenchmarkModule("DecodeBenchmark", "For only the decode part of the challenge solving")]
+public class DecodeOnly
+{
+    [Benchmark]
+    public void Decode()
+    {
+        Span<char> buffer = stackalloc char[256];
+
+        _ = Challenge.Decode(Mock.Token, buffer, Mock.Offset);
+    }
+}
